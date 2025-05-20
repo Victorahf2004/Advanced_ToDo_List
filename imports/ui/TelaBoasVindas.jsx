e7663bf2248bddc9653159f15d9a568506da4985
Victorahf2004 <honfonseca@gmail.com>
2025-05-20 10:48:32 -0300

    Drawer Implementado

--- imports/ui/TelaBoasVindas.jsx
+++ imports/ui/TelaBoasVindas.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useTracker } from "meteor/react-meteor-data";
 import { Meteor } from "meteor/meteor";
@@ -6,11 +6,46 @@ import Button from "@mui/material/Button";
 import Alert from "@mui/material/Alert";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Avatar from "@mui/material/Avatar";
+import ButtonBase from "@mui/material/ButtonBase";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from '@mui/icons-material/Menu';
+import Tooltip from "@mui/material/Tooltip";
 
 export const TelaBoasVindas = ({erroLogout, setErroLogout}) => {
     const user = useTracker(() => Meteor.user());
     let navigate = useNavigate();
+    const [openDrawer, setOpenDrawer] = useState(false);
 
+    if (!user) {
+        return (
+            <div>
+                Loading...
+            </div>
+        )
+    }
+    
+    const atributosUsuario = [user.profile["nome"], user.profile["email"]];
+    
+    const openPerfil = () => {
+        navigate("/Logado/Perfil");
+        setErroLogout(false);
+        setOpenDrawer(false);
+    }
+
+    const openTasks = () => {
+        navigate("/Logado/ListaTasks");
+        setErroLogout(false);
+        setOpenDrawer(false)
+    }
+    
     const logoutAsync = () => {
         new Promise((resolve, reject) => {
             Meteor.logout((err) => {
@@ -25,20 +60,62 @@ export const TelaBoasVindas = ({erroLogout, setErroLogout}) => {
             await logoutAsync();
             navigate("/");
             setErroLogout(false);
+            setOpenDrawer(false);
         }
         catch (error) {
             setErroLogout(true);
+            setOpenDrawer(false);
         }
     }
-    const openTasks = () => {
-        navigate("/Logado/ListaTasks");
-        setErroLogout(false);
-    };
 
-    const openPerfil = () => {
-        navigate("/Logado/Perfil");
-        setErroLogout(false);
+    const toggleDrawer = (newState) => {
+        setOpenDrawer(newState);
     }
+
+    const listaDrawer = (
+            <Box onClick={() => toggleDrawer(false)}>
+            <List>
+                <ListItem>
+                    <ListItemButton onClick={openPerfil}>
+                        <Avatar alt="Foto de perfil" src={user.profile["foto"]} />
+                    </ListItemButton>
+                </ListItem>
+                {atributosUsuario.map((atributo, i) => (
+                    <React.Fragment key={i}>
+                        {atributo? (
+                            <ListItem>
+                                <ListItemButton onClick={openPerfil}>
+                                    <ListItemText primary={atributo} />
+                                </ListItemButton>
+                            </ListItem>
+                        ): (
+                            <>
+                            </>
+                        )
+                        }
+                    </React.Fragment>
+                ))}
+                <Divider />
+                <ListItem>
+                    <ListItemButton onClick={openPerfil}>
+                        <ListItemText primary="Abrir Perfil" />
+                    </ListItemButton>
+                </ListItem>
+                <Divider />
+                <ListItem>
+                    <ListItemButton onClick={openTasks}>
+                        <ListItemText primary="Abrir Tasks" />
+                    </ListItemButton>
+                </ListItem>
+                <Divider />
+                <ListItem>
+                    <ListItemButton onClick={logout}>
+                        <ListItemText primary="Log Out" />
+                    </ListItemButton>
+                </ListItem>
+            </List>
+            </Box>
+        )
 
     const ajustarDisplay = (parametro) => {
         let tamanho = parametro.length;
@@ -49,17 +126,23 @@ export const TelaBoasVindas = ({erroLogout, setErroLogout}) => {
             return parametro;
         }
     }
+
     return (
         <>
         {erroLogout && (
                     <Alert severity="error" onClose={() => {setErroLogout(false);}} > Erro no Logout</Alert>
                 )}
+        <Tooltip title="Menu">
+            <IconButton onClick={() => toggleDrawer(true)}>
+                <MenuIcon />
+            </IconButton>
+        </Tooltip>
         <Typography variant="h3" gutterBottom>
             Seja Bem-Vindo, {ajustarDisplay(user.username)}!!!
         </Typography>
-        <Button variant="contained" onClick={openTasks}>Abrir Tasks</Button>
-        <Button variant="contained" onClick={logout}>Log Out</Button>
-        <Button variant="contained" onClick={openPerfil}>Abrir Perfil</Button>
+        <Drawer open={openDrawer} onClose={() => toggleDrawer(false)}>
+            {listaDrawer}
+        </Drawer>
         </>
     )
 }
